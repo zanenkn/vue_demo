95dc3b7f60a9264fe6a96dd2e3b5567b34fa1432
zanenkn <zane.neikena@gmail.com>
2019-08-13 10:16:27 +0200

    Creates Rating component and imports it into main.js

---
+++ src/components/Rating.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>
+</template>
+
+<script>
+  /* Your JS goes here */
+</script>
+
+<style>
+  /* Your CSS goes here */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Rating from './components/Rating'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  template: '<Rating/>',
+  components: { Rating }
 })
