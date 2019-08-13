fd9ebe9804a74a039847e6e0f62a8250f24d21f4
zanenkn <zane.neikena@gmail.com>
2019-08-13 14:26:22 +0200

    Styling Rating component.

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
     <ul>
-      <li><icon name="star" /></li>
-      <li><icon name="star" /></li>
-      <li><icon name="star" /></li>
-      <li><icon name="regular/star" /></li>
-      <li><icon name="regular/star" /></li>
+      <li class="star active"><icon name="star" /></li>
+      <li class="star active"><icon name="star" /></li>
+      <li class="star active"><icon name="star" /></li>
+      <li class="star"><icon name="regular/star" /></li>
+      <li class="star"><icon name="regular/star" /></li>
     </ul>
     <span>3 of 5</span>
   </div>
-
 </template>
 
 <script>
@@ -23,6 +22,28 @@
   }
 </script>
 
-<style>
-  /* Your CSS goes here */
+<style scoped>
+  .rating {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #a7a8a8;
+  }
+  .list {
+    margin: 0 0 5px 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  .list:hover .star {
+    color: #f3d23e;
+  }
+  .star {
+    display: inline-block;
+    cursor: pointer;
+  }
+  .star:hover ~ .star:not(.active) {
+    color: inherit;
+  }
+  .active {
+    color: #f3d23e;
+  }
 </style>
